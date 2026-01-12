df5229e2b7f6af7b6bc112982a41657c6b08f7d6
unknown <caohuie0824@gmail.com>
2026-01-12 22:02:26 +0700

    create SalaryRecord

--- HumanResourceManagementSystem/src/Entities/SalaryRecord.java
+++ HumanResourceManagementSystem/src/Entities/SalaryRecord.java
@@ -9,5 +9,85 @@ package Entities;
  * @author NGOMI
  */
 public class SalaryRecord {
-    
+
+    private String employeeId;
+    private int month;
+    private int year;
+    private int workingDays;
+    private int absenceDays;
+    private int overtimeHours;
+    private double totalSalary;
+
+    public SalaryRecord() {
+    }
+
+    public SalaryRecord(String employeeId, int month, int year, int workingDays, int absenceDays, int overtimeHours, double totalSalary) {
+        this.employeeId = employeeId;
+        this.month = month;
+        this.year = year;
+        this.workingDays = workingDays;
+        this.absenceDays = absenceDays;
+        this.overtimeHours = overtimeHours;
+        this.totalSalary = totalSalary;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getWorkingDays() {
+        return workingDays;
+    }
+
+    public void setWorkingDays(int workingDays) {
+        this.workingDays = workingDays;
+    }
+
+    public int getAbsenceDays() {
+        return absenceDays;
+    }
+
+    public void setAbsenceDays(int absenceDays) {
+        this.absenceDays = absenceDays;
+    }
+
+    public int getOvertimeHours() {
+        return overtimeHours;
+    }
+
+    public void setOvertimeHours(int overtimeHours) {
+        this.overtimeHours = overtimeHours;
+    }
+
+    public double getTotalSalary() {
+        return totalSalary;
+    }
+
+    public void setTotalSalary(double totalSalary) {
+        this.totalSalary = totalSalary;
+    }
+    public double caculatorSalary()
+    {
+        return 0;
+    }
 }
