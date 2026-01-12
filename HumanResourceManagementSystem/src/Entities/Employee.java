3054837f0d7b71770a12268fd9bb25edcf734d56
Harry-Cuong <Vietbacuong@gmail.com>
2026-01-12 22:12:11 +0700

    new employee

--- HumanResourceManagementSystem/src/Entities/Employee.java
+++ HumanResourceManagementSystem/src/Entities/Employee.java
@@ -4,10 +4,39 @@
  */
 package Entities;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author NGOMI
  */
 public class Employee {
-    
+    String employeeId;      
+    String fullName;        
+    String department;      
+    String jobTitle;       
+    LocalDate dateOfJoining;
+    double basicSalary;     
+    String employmentType;  
+    String status;
+
+    public Employee(String id, String name, String dept, double salary) {
+        this.employeeId = id;
+        this.fullName = name;
+        this.department = dept;
+        this.basicSalary = salary;
+        this.status = "Đang làm việc"; 
+    }
+
+ 
+    public void updateInfo(String newName, double newSalary) {
+        this.fullName = newName;
+        this.basicSalary = newSalary;
+        System.out.println("Đã cập nhật xong cho nhân viên: " + this.employeeId);
+    }
+
+    @Override
+    public String toString() {
+        return "Nhân viên: " + fullName + " - Phòng: " + department + " - Lương: " + basicSalary;
+    }
 }
